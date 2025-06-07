7ada23e46146c5591b7c2b9ce633ace339397ed1
dondish <odedshapira1@protonmail.com>
2025-06-07 16:26:11 +0300

    Add tree sitter requirements

---
+++ lua/plugins/tree-sitter.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "cpp",
+        "gn",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "rust",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+    },
+  },
+}
